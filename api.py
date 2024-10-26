70ba45f77eca6fcf2d5ee19943b8ecc43b137db0
Nikhil <agastyakarthik29@gmail.com>
2024-10-26 12:53:46 +0530

    added host

--- api.py
+++ api.py
@@ -84,4 +84,4 @@ def compare_faces():
         return jsonify({"match": False, "distance": face_distance[0]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0' , debug=True, port=5000)
